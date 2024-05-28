a27210b7e21382d1b53492ae7242017e4396906c
Ollie-Branch <ollieevelon@gmail.com>
2024-05-28 11:30:34 -0400

    updated float shredder to pascal case version

--- float_analyzer.cpp
+++ float_analyzer.cpp
@@ -12,17 +12,17 @@ int main (int argc, char** argv) {
 
 	std::cout << "The binary representation of your float is: " << std::endl;
 	std::cout 
-		<< std::bitset<32>(shredder_raw_float_data(input_float)) 
+		<< std::bitset<32>(ShredFloatToData(input_float)) 
 		<< std::endl;
 	std::cout << "" << std::endl;
 
 	std::cout << 
 		"The binary representation of the float's unbiased exponent is: " 
 		<< std::endl;
-	std::cout << std::bitset<8>(shredder_float_exp_unbiased(input_float)) << std::endl;
+	std::cout << std::bitset<8>(ShredFloatExpUnbiased(input_float)) << std::endl;
 	std::cout << "The decimal representation of the float's unbiased exponent is: "
 		<< std::endl;
-	std::cout << shredder_float_exp_unbiased(input_float) << std::endl;
+	std::cout << ShredFloatExpUnbiased(input_float) << std::endl;
 	std::cout << "" << std::endl;
 
 	std::cout 
@@ -35,18 +35,18 @@ int main (int argc, char** argv) {
 	std::cout << "" << std::endl;
 
 	std::cout << "The float's true (biased) exponent is: " << std::endl;
-	std::cout << shredder_float_exp_biased(input_float) << std::endl;
+	std::cout << ShredFloatExp(input_float) << std::endl;
 	std::cout << "" << std::endl;
 
 	std::cout << 
 		"The binary value of the mantissa (the fractional part of the number in binary scientific notation)"
 		<< std::endl;
-	std::cout << std::bitset<23>(shredder_raw_float_mantissa(input_float)) << std::endl;
+	std::cout << std::bitset<23>(ShredFloatMantissaRaw(input_float)) << std::endl;
 	std::cout << "The decimal value of the mantissa is: " << std::endl;
-	std::cout << shredder_mantissa(input_float) << std::endl;
+	std::cout << ShredFloatMantissa(input_float) << std::endl;
 	std::cout << "The 1 is implicit if the raw exponent value is greater than 0" << std::endl;
 	std::cout << "" << std::endl;
 
 	std::cout << "Approximate maximum that can represent this fractional value: " << std::endl;
-	std::cout << std::pow(2, (24 + (shredder_float_exp_biased(input_float)))) << std::endl;
+	std::cout << std::pow(2, (24 + (ShredFloatExp(input_float)))) << std::endl;
 }
